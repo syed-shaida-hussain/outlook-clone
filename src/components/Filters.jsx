039e2c7b7a1f120111965c66f70cd3938f248c26
syed-shaida-hussain <85978159+syed-shaida-hussain@users.noreply.github.com>
2024-10-13 15:38:23 +0530

    Feat : Added reset filter button

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -3,12 +3,13 @@ import { useEmails } from "../contexts/EmailContext"
 const Filters = () => {
   const {setFilterData , filterData} = useEmails();
   return (
-    <section className="flex gap-20 filters">
+    <section className="flex gap-15 filters">
     <span>Filter By:</span>  
-    <div className="flex gap-15">
+    <div className="flex gap-5">
          <button className={filterData?.showUnreadOnly ? "active-btn": ""} onClick={() => setFilterData({...filterData , showReadOnly : false , showFavouritesOnly : false , showUnreadOnly : true})}>Unread</button>
          <button className={filterData?.showReadOnly ? "active-btn": ""} onClick={() => setFilterData({...filterData , showReadOnly : true , showFavouritesOnly : false , showUnreadOnly : false})}>Read</button>
          <button className={filterData?.showFavouritesOnly ? "active-btn" : ""} onClick={() => setFilterData({...filterData , showReadOnly : false , showFavouritesOnly : true , showUnreadOnly : false})}>Favourites</button>
+         <button  onClick={() => setFilterData({...filterData , showReadOnly : false , showFavouritesOnly : false , showUnreadOnly : false})}>Reset</button>
     </div>
  </section>
   )
